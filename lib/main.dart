1b1e420944beb0152c6106dbbfa7c532aa668733
Julien SAUMANDE <Julien@MacBook-Air-5311.local>
2020-01-15 23:40:20 +0100

    xxx

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:tinderlike/cardItem.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutter/services.dart' show rootBundle;
 
 import 'deckCard.dart';
 
